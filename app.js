16dbe11bde5a0c88e33068c2a1861ee37ca8ce2e
LuukScheur <luukvdscheur1@hotmail.com>
2014-10-19 17:19:42 +0200

    game is working

--- app.js
+++ app.js
@@ -3,11 +3,19 @@ var path        = require('path');
 var http        = require('http');
 var socketio    = require('socket.io');
 
+/*
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://127.0.0.1:27017/kittyDB');
+var db = mongoose.connection;
+*/
+
 var app        = express();
 var httpServer = http.Server(app);
 var io         = socketio(httpServer);
 
 app.use(express.static(path.join(__dirname, 'client')));
+
+//require('security.js')(app); //install security to the app
 
 app.get('/', function(req, res){
   res.sendfile('index.html');
@@ -18,25 +26,53 @@ app.get('/', function(req, res){
 /* * * * * * * * * * * * * * * * * * * * * * */
 
 var playerList = [];
+var gameActive = false;
 io.on("connection", function(socket){
    console.log("User Connected", socket.id);
    
+   // if there is a new user:
    socket.on("sign in", function(data) {
-      var newPlayer = {
-		 id: socket.id,
-         name: data.name
-      }
-	  
-	  if( playerList.length < 1) { isAdmin = true; } else { isAdmin = false; }
-	  
-      socket.emit("sign in reply", {admin: isAdmin, player: newPlayer});
-      socket.broadcast.emit("new user", newPlayer);
-      playerList.push(newPlayer);
+      if(gameActive) {
+		socket.emit("sign in reply", {error: true, message: 'Game is already active'});
+	  } else {
+		  var newPlayer = {
+			 id: socket.id,
+			 name: data.name,
+			 answers: []
+		  }
+		  
+		  // the first user will be a admin (isAdmin = true)
+		  if( playerList.length < 1) { isAdmin = true; } else { isAdmin = false; }
+		
+		  socket.emit("sign in reply", {admin: isAdmin, player: newPlayer});
+		  socket.broadcast.emit("new user", newPlayer);
+		  playerList.push(newPlayer);
+	  }
    });
+   // admin logs in and recieves the playerList
    socket.on("adminLogin", function() {
 		socket.emit('adminLogin reply', {list: playerList});
    });
+   // if user has filled in a answer 
+   socket.on('userAnswered', function(answer){
+	for(var i = 0; i < playerList.length; i++) {
+		if(playerList[i].id === answer.playerID) {
+			// insert answer in server playerList
+			playerList[i].answers[answer.questionNr] = answer.answer;
+		}
+	}
+	// send the answer to the admin
+	socket.broadcast.emit('userAnswer', answer);
+   });
    
+   // if the admin uploads the questionList (game is starting!)
+   socket.on('listUpload', function(questionList) {
+		gameActive = true;
+		socket.broadcast.emit('listRecieved', questionList);
+		//console.log('questionList recieved: ', questionList);
+   });
+   
+   // for a new user, recieving user and playerList
    socket.on("getMyInfo", function(user) {
 		for(var i = 0; i < playerList.length; i++) {
 		   if(playerList[i].name === user.name) {
--- client/js/main.js
+++ client/js/main.js
@@ -1,14 +1,20 @@
-var app = angular.module("SockBlocksApp",["ngRoute"]);
+/**
+ * Author: Luuk van de Scheur, David Stam
+ * Date: 19-10-2014
+ *
+ * jslint browser: false
+ * jslint devel: true
+ * jslint node: true
+ */
+/*global window, document, location*/
+
+var app = angular.module("davukApp",["ngRoute"]);
 
 app.config( function($routeProvider) {
       $routeProvider.
           when('/login', {
               templateUrl: '/templates/loginForm.html',
               controller: 'loginFormController'
-          }).
-		  when('/config/:name', {
-			  templateUrl: '/templates/loginForm.html',
-              controller: 'configController'
           }).
 		  when('/adminPanel', {
               templateUrl: '/templates/adminPanel.html',
@@ -23,6 +29,7 @@ app.config( function($routeProvider) {
           });
 });
 
+// I don't know what this does but its necessary:
 app.factory('socketIO', function ($rootScope) {
   var socket = io();
   socket.on("connect", function() {
@@ -54,37 +61,80 @@ app.factory('socketIO', function ($rootScope) {
 });
 
 // LOGIN FORM FOR ALL USERS
-app.controller("loginFormController", function($scope, $location) {
+app.controller("loginFormController", function($scope, $routeParams, socketIO, $location) {
    $scope.userName = "";
-   $scope.submitUserName = function() {
-      $location.path("/config/" + $scope.userName);
-   }
-});
-// REGISTER/CREATE A USER/ADMIN
-app.controller("configController", function($scope, $routeParams, $location, socketIO) {
+   $scope.error = false;
    
-   socketIO.emit("sign in", { name: $routeParams.name} )
-   socketIO.on("sign in reply", function(reply) {
-	
-	if(reply.admin) { $location.path("/adminPanel"); }
-	else { $location.path("/game/" + reply.player.name); }
-	
-   });
- 
+   // submit user to the server and redirect to the correct page (admin/player)
+   $scope.submitUserName = function() {
+	   socketIO.emit("sign in", { name: $scope.userName} )
+	   socketIO.on("sign in reply", function(reply) {
+		if(reply.error) {
+			$scope.message = reply.message;
+			$scope.error = reply.error;
+			
+		} else {
+			if(reply.admin) { $location.path("/adminPanel"); } // = admin
+			else { $location.path("/game/" + reply.player.name); } // = player
+		}
+		
+	   });
+   }
 });
 
 // THIS IS A ADMIN LOGGED IN:
 app.controller("adminPanelController", function($scope, $location, socketIO) {
    $scope.playerList = [];
-   $scope.player = null;
+   $scope.admin = null;
+   $scope.error = false;
+   $scope.maxConnected = 30; // max users connected (server allows more @TODO)
+   $scope.selectedList = [];
+   $scope.questionList = [
+   {id: 1, question: 'Hoe oud ben jij?'},
+   {id: 2, question: 'Wat is David zijn achternaam?'},
+   {id: 3, question: 'Had je een grote bek?'},
+   {id: 4, question: 'Wat is een ginger?'},
+   {id: 5, question: 'Is het antwoord A, B of C?'},
+   {id: 6, question: 'Hoe lang is een chinees?'}
+   ]; // this should be taken out of the database.
+
+   // submit the selected questions to the players (game is starting!)
+   $scope.submitQuestions = function() {
+		var inputs = document.getElementsByTagName('input');
+		for (var i = 0; i < inputs.length; i++) {
+			if(inputs[i].checked) {
+				$scope.selectedList.push($scope.questionList[i]);
+			}
+		}
+		if($scope.selectedList[0]){
+			socketIO.emit('listUpload', $scope.selectedList); // this is starting the game
+		} else {
+			$scope.error = true;
+			$scope.message = 'Selecteer minstens 1 vraag!';
+		}
+   }
    
+   // if user answers a question, update answer table and update local playerList var
+   socketIO.on('userAnswer', function (answer) {
+	for(var i = 0; i < $scope.playerList.length; i++) {
+		if($scope.playerList[i].id === answer.playerID) {
+			$scope.playerList[i].answers[answer.questionNr] = answer.answer;
+			if(!answer.answer || answer.answer == null) { answer.answer = '<b> NULL </b>'; }
+			document.getElementById($scope.playerList[i].id + (answer.questionNr + 1)).innerHTML = answer.answer;
+		}
+	}
+   });
+   
+   // login admin.
    socketIO.emit('adminLogin');
+   // login admin reply (user and playerList)
    socketIO.on("adminLogin reply", function(gameInfo) {
 	   console.log('gameInfo: ', gameInfo);
-	   $scope.player = gameInfo.list[0];
+	   $scope.admin = gameInfo.list[0];
 	   $scope.playerList = gameInfo.list;
    });
    
+   // when a new user connects update playerList
    socketIO.on("new user", function(playerInfo) {
       console.log("NEWUSER:", playerInfo);
       $scope.playerList.push(playerInfo);
@@ -94,21 +144,78 @@ app.controller("adminPanelController", function($scope, $location, socketIO) {
 
 
 // THIS IS A STUDENT LOGGED IN:
-app.controller("gameController", function($scope, $routeParams, $location, socketIO) {
+app.controller("gameController", function($scope, $interval, $timeout, $routeParams, $location, socketIO) {
 
    $scope.playerList = [];
    $scope.player = null;
+   $scope.questionList = [];
+   $scope.questionTime = 5; // in seconds
+   $scope.question = 0; //index of questionList
    
+   // game is starting, recieving list..
+   socketIO.on('listRecieved', function(questionList) {
+	$scope.questionList = shuffle(questionList); //randomize the questionList for each player (see shuffle function)
+	askQuestion(); //ask the first question of the array
+	if($scope.questionList.length !== 1){
+		// ask the next question every questionTime seconds until array is complete
+		$interval(askQuestion, ($scope.questionTime + 1)*1000, $scope.questionList.length-1);
+	}
+   });
+   
+   function askQuestion() {
+	$scope.question++;
+	console.log('asking question..', $scope.question);
+    $scope.timeLeft = $scope.questionTime;
+	$interval( function(){$scope.timeLeft--;} , 1000, $scope.questionTime  ); // reduce time each seconds
+	$timeout(saveAnswer, ($scope.questionTime+0.9)*1000);
+   }
+   
+   function saveAnswer() {
+	console.log('saving answer..', $scope.question);
+	var theAnswer = document.getElementById('theAnswer'); //get the answer with plain javascript
+	$scope.player.answers[ ($scope.questionList[$scope.question -1].id) -1] = theAnswer.value; //save answer to local player var
+	socketIO.emit('userAnswered', {playerID: $scope.player.id, questionNr: $scope.questionList[$scope.question -1].id -1, answer: theAnswer.value}); //send answer to server
+	theAnswer.value = ''; //reset answer
+   }
+   
+   // login player
    socketIO.emit('getMyInfo', { name: $routeParams.name });
+   // login player reply
    socketIO.on("getMyInfo reply", function(gameInfo) {
 	   console.log('gameInfo: ', gameInfo);
 	   $scope.player = gameInfo.player;
 	   $scope.playerList = gameInfo.list;
    });
    
+   // if new user update the playerList (player doesn't use the playerList though)
    socketIO.on("new user", function(playerInfo) {
       console.log("NEWUSER:", playerInfo);
       $scope.playerList.push(playerInfo);
    });
 });
+
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex ;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+function check (div) {
+	document.getElementById('question'+div.substring(div.length, div.length -1)).checked = !document.getElementById('question'+div.substring(div.length, div.length -1)).checked;
+	console.log();
+}
 
